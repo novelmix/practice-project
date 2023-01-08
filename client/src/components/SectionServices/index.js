132e5cad4b801d3357760a79b0cc652d43b7e33f
AndrewS <andreyspace12@gmail.com>
2023-01-08 16:54:02 +0200

    fixed the code

--- client/src/components/SectionServices/index.js
+++ client/src/components/SectionServices/index.js
@@ -5,89 +5,89 @@ import { Link } from 'react-router-dom';
 const SectionServices = () => {
   return (
     <section className={styles['services-container']}>
-      <div>
+      <actircle>
         <h2 className={styles['services-title']}>All Branding Agency Services</h2>
         <p className={styles['services-sub-title']}>Let out Squad of 250K+ experts from around the world help you launch your brand!</p>
-      </div>
+      </actircle>
       <div className={styles['services-container-card']}>
         <Link to="/">
           <div className={styles['services-card']}>
-          <div>
+          <actircle>
           <h3><ion-icon name="star"></ion-icon>Premium Domains</h3>
           <p>Curated drandable domains available for immediaote purchase</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="people"></ion-icon>Naming Contests</h3>
             <p>Crowdsourced Name ideas from our creative community</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="person"></ion-icon>Managed Contests</h3>
             <p>Full Agency-level Experience, led by Branding Consultant</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="pricetags"></ion-icon>Logo Contests</h3>
             <p>Professional logo design contests</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="chatbubbles"></ion-icon>Tagline Contests</h3>
             <p>Taglines, slogan, and strapline ideas from our creative community</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="videocam"></ion-icon>Video Creation</h3>
             <p>Get perfectly branded videos optimized for results</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="bar-chart"></ion-icon>Audience Testing</h3>
             <p>Fast, effective naming and branding research with consumer test panels</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="bag-check"></ion-icon>Trademark Research</h3>
             <p>Comprehensive Trademark Research across 100+ countries</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
         <Link to="/">
         <div className={styles['services-card']}>
-          <div>
+          <actircle>
             <h3><ion-icon name="information-circle"></ion-icon>Trademark Filing</h3>
             <p>Trademark filing service by Licensed Trademark attorneys</p>
-          </div>
+          </actircle>
           <ion-icon name="chevron-forward"></ion-icon>
         </div>
         </Link>
